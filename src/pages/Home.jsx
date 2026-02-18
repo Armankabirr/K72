7d7d9b61806272a5e24b53beb5b4eec3e7bb80be
Armankabirr <armanutsho12@gmail.com>
2026-02-18 22:58:04 +0600

    added hero text with video in text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Video from '../components/home/video'
+import HomeHeroText from './../components/home/HomeHeroText';
+import HomeBottomText from './../components/home/HomeBottomText';
 
 const Home = () => {
   return (
     <div>
-      <div className='h-screen w-screen bg-red-900'>
+      <div className='h-screen w-screen fixed bg-red-900'>
           <Video />
+      </div>
+      <div className='h-screen w-screen relative flex flex-col'>
+          <HomeHeroText />
+          <HomeBottomText />
       </div>
     </div>
   )
