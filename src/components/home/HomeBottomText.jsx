38d325c316df32b82d96e569df09c77f4b69fb8f
Armankabirr <armanutsho12@gmail.com>
2026-02-18 23:25:53 +0600

    added home page hero section

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>
-      
+    <div className='font-[font2] flex items-center justify-center gap-2 '>
+        <Link className='text-[6.5vw] leading-[6.5vw] border-2 border-white rounded-full px-10 py-5 uppercase pt-1 pb-0 '>Projects</Link>
+        <Link className='text-[6.5vw] leading-[6.5vw] border-2 border-white rounded-full px-10 py-5 uppercase pt-1 pb-0 '>agence</Link>
     </div>
   )
 }
