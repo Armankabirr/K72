4ca4b9e5962b8f8761fb0cbc1012b6cd404171d0
Armankabirr <armanutsho12@gmail.com>
2026-02-19 20:51:14 +0600

    changed bottom text layout

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='font-[font2] flex items-center justify-center gap-2 '>
-        <Link className='text-[6.5vw] leading-[6.5vw] border-2 border-white rounded-full px-10 py-5 uppercase pt-1 pb-0 '>Projects</Link>
-        <Link className='text-[6.5vw] leading-[6.5vw] border-2 border-white rounded-full px-10 py-5 uppercase pt-1 pb-0 '>agence</Link>
+        <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-44 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] lg:mt-6' to='/projects'>Projects</Link>
+      </div>
+      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-44 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] lg:mt-6' to='/agence'>agence</Link>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='h-screen w-screen fixed bg-red-900'>
           <Video />
       </div>
-      <div className='h-screen w-screen relative flex flex-col justify-between'>
+      <div className='h-screen w-screen pb-5 overflow-hidden relative flex flex-col justify-between'>
           <HomeHeroText />
           <HomeBottomText />
       </div>
